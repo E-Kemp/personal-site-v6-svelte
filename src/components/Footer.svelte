6f149f54b3ff0826398c3c7b096037e3caf3df47
E-Kemp <35563838+E-Kemp@users.noreply.github.com>
2025-01-03 18:26:48 +0000

    Added new footer content

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,15 @@
+<script lang="ts">
+	import Button from './Button.svelte';
+
+    const currentYear = new Date().getFullYear()
+</script>
+
 <footer
 	class="flex h-16 w-full flex-row items-center justify-end gap-6 bg-gunmetal px-4 text-beige"
 >
-	<p>Powered by Svelte</p>
+	<p>&copy; {currentYear} elliotjordankemp.com</p>
 	<span>&bull;</span>
-	<p>Check out the code!</p>
+	<a href="https://github.com/E-Kemp/personal-site-v6-svelte"
+		><Button small>Check out the code!</Button></a
+	>
 </footer>
